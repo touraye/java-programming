c0fbbc60a26dadab0cdbd835ce585b881269e3ab
touraye <touraye07@gmail>
2025-02-23 18:38:25 +0000

    added semester 2025

---
+++ semester-2025/java1/assignment/assgn1/Counting.java
@@ -0,0 +1,20 @@
+package java1.assignment.assgn1;
+
+public class Counting {
+    public static void main(String[] args) {
+        // Utilize the numbers from
+        /*
+         Make use of all the operator you have learned from
+         Mathematics: Addition, Subtraction, Multiplication, and Division
+         Create two variable of type int namely; a and b and assigned them any
+         of your desire value. Perform each operation in a separate print
+         statement(System.out.println(a*b);). Your assignment is expected to have
+         four print statements each doing specific operation. At the end of the day
+         your program should display the result of each operation in the console.
+         */
+        int a = 10;
+        int b = 2;
+        int c = a * b;
+        System.out.println(a*b);
+    }
+}
---
+++ semester-2025/java1/assignment/assgn1/PersonalInfo.java
@@ -0,0 +1,25 @@
+package java1.assignment.assgn1;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+        // personal information
+        String name = "Ahmed Dicko";
+        int age = 18;
+        String address = "Jam city";
+        boolean marriageStatus = false;
+        String nationality = "Gambian";
+        char gender = 'M';
+        String school = "Management Development Institute";
+
+        // print personal info
+        System.out.println("************* My Personal Information *************");
+        System.out.println("Name: " +name);
+        System.out.println("Age: " +age);
+        System.out.println("Address: " +address);
+        System.out.println("Marriage Status: " +marriageStatus);
+        System.out.println("Nationality: " +nationality);
+        System.out.println("Gender: " +gender);
+        System.out.println("School: " +school);
+    }
+}
+
